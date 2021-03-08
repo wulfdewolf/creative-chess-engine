3dc1f4a5b8928d3b3025fd6b7c55622df8a84060
wulfdewolf <wolf2wulf@gmail.com>
2021-03-08 13:52:21 +0100

    Changed infrastructure, ready to implement CGF

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 #------------------------------------------------
 import chess
 import chess.engine
+from optimality import get_optimality_scores
+from creativity import get_creativity_scores
 
 # Initial board
 initial_board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
@@ -14,43 +16,9 @@ initial_board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 # Engine location
 engine = chess.engine.SimpleEngine.popen_uci('./Stockfish/src/stockfish')
 
-# OPTIMALITY SCORES
-optimality_scores = {}
-
-# CREATIVITY SCORES
-creativity_scores = {}
-
-
-def get_optimality_scores(fen):
-
-    # Parse board
-    board = chess.Board(fen)
-
-    # Dict with results
-    scores = {}
-
-    # Get scores for given board
-    for el in board.legal_moves:
-        info = engine.analyse(board, chess.engine.Limit(time=.1), root_moves=[el])
-
-        # Calculate score
-        score = 0.0
-        if(board.turn):
-            score = info["score"].white().score()
-        else:
-            info["score"].black().score()
-        score = round(score/100,2)
-
-        # Store in dict
-        scores[el.uci()] = score
-
-    # Return scores
-    return scores
-
-
 
 #-------------------------------------
 #               MAIN
 #-------------------------------------
-print(get_optimality_scores(initial_board))
+print(get_optimality_scores(initial_board, engine))
 engine.quit()
---
+++ optimality.py
@@ -0,0 +1,37 @@
+#------------------------------------------------
+#
+#           A CREATIVE CHESS ENGINE
+#
+#             author: Wolf De Wulf
+#
+#------------------------------------------------
+import chess
+import chess.engine
+
+
+# Given a board and an engine calculate the optimality scores of all the legal moves
+def get_optimality_scores(fen, engine):
+
+    # Parse board
+    board = chess.Board(fen)
+
+    # Dict with results
+    scores = {}
+
+    # Get scores for given board
+    for el in board.legal_moves:
+        info = engine.analyse(board, chess.engine.Limit(time=.1), root_moves=[el])
+
+        # Calculate score
+        score = 0.0
+        if(board.turn):
+            score = info["score"].white().score()
+        else:
+            info["score"].black().score()
+        score = round(score/100,2)
+
+        # Store in dict
+        scores[el.uci()] = score
+
+    # Return scores
+    return scores
