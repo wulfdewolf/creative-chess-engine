e4353c3caf1b23590dd5b46ea664842b6ad99f3e
wulfdewolf <wolf2wulf@gmail.com>
2021-04-02 11:07:06 +0200

    Added results and games from learning phase

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,10 +1,11 @@
 
 library(ggplot2)
 
-data <- read.csv("~/Documents/studies/2/CC/creative-chess-engine/analysis/creative_engine_learnt.csv", header=FALSE)
+data <- read.csv("~/Documents/studies/2/CC/creative-chess-engine/analysis/white_learnt.csv", header=FALSE)
 
-ggplot(data, aes(x = seq(1, 222))) +
-  geom_line(aes(y = V1, colour = "Unkown move")) +
+# Evolution of the weights
+ggplot(data, aes(x = seq(1, 1000))) +
+  geom_line(aes(y = V1, color = "Unkown move")) +
   geom_line(aes(y = V2, color = "Low winrate")) + 
   geom_line(aes(y = V3, color = "Suboptimal capture")) +
   geom_line(aes(y = V4, color = "Sacrifice")) + 
@@ -12,3 +13,6 @@ ggplot(data, aes(x = seq(1, 222))) +
   xlab("Game") + 
   ylab("Weight") + 
   labs(colour = "Type")
+
+# Histogram game results
+barplot(table(data$V6), xlab="Game result", ylab="Games")
