ee6657d218cb4c70a4292554cae53c920680c2ab
wulfdewolf <wolf2wulf@gmail.com>
2021-04-24 17:04:11 +0200

    fixed play.py input args

--- play.py
+++ play.py
@@ -23,8 +23,6 @@ if(len(sys.argv) != 2):
 else:
     color_engine = chess.WHITE if(str(sys.argv[1]) == "WHITE") else chess.BLACK
 
-color_engine = "BLACK"
-
 # Create a creative engine
 creative_engine = CreativeChessEngine("engine", heuristics_engine, [3.9899999999999998,2.819999999999975,2,6.819999999999901,(9.209999999999884/2)] if(color_engine) else [3.6999999999999877,3.2199999999999664,2,6.519999999999899,(9.449999999999877/2)])
 
@@ -49,7 +47,7 @@ try:
 
     while(not(creative_engine.game_done())):
 
-        if(color_engine == "WHITE"):
+        if(color_engine):
 
             # Let the engine play
             print("Engine move: ")
